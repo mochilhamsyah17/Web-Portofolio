167bbae7030d0bf4d9fac0d0922d182714a7e197
mochilhamsyah17 <mochilhamsyah17@gmail.com>
2024-09-03 22:23:45 +0700

    update:project responsive

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -4,7 +4,7 @@ import React from "react";
 
 export default function Navbar() {
   return (
-    <section className="flex gap-x-10 w-full py-4 justify-center bg-[#222222] text-xs md:text-sm">
+    <section className="flex gap-x-1 lg:gap-x-10 w-full py-4 justify-center bg-[#222222] text-xs md:text-sm">
       <button className="hover:bg-[#191919] py-1 px-2 rounded-lg">
         <a href="#experienced-tools">
           <span>Experienced Tools</span>
